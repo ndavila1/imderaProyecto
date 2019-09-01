c079b868797c2fc66df5763000ca852b4734d635
Nicolas Davila <nikojako99@gmail.com>
2019-09-01 08:46:14 -0500

    Adelanto projecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { EscenariosComponent } from './components/escenarios/escenarios.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { Page404Component } from './components/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent 
+  },
+  {
+    path: 'Herramientas/Escenario',
+    component:EscenariosComponent // only user auth
+  },
+  {
+    path:'Home',
+    component:HomeComponent
+  },
+  {
+    path:'**',
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,37 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EscenariosComponent } from './components/escenarios/escenarios.component';
 import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
 import { AngularFirestoreModule, AngularFirestore } from "@angular/fire/firestore";
 import { environment } from "../environments/environment";
+import { AngularFireAuth } from "@angular/fire/auth";
+
+
+import { EscenariosComponent } from './components/escenarios/escenarios.component';
+import { HomeComponent } from "./components/home/home.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EscenariosComponent
+    EscenariosComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireDatabaseModule
   ],
-  providers: [AngularFirestore],
+  providers: [AngularFirestore,AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
